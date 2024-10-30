6859ac19c841040ce14409e90346a04f84d4a21d
pRs3k <160817153+pRs3k@users.noreply.github.com>
2024-10-29 21:14:30 -0600

    v0.1.0
    -Successfully spins the motor and plays music simultaneously

--- JitterGhost.py
+++ JitterGhost.py
@@ -2,31 +2,59 @@
 
 from machine import Pin, UART, PWM
 import time
+from micropython_dfplayer import DFPlayer
+from time import sleep
 
 # Pin setup
-pir_sensor = Pin(16, Pin.IN)           # PIR sensor connected to GPIO 16
-led = Pin(15, Pin.OUT)                 # LED connected to GPIO 15
-motor_pwm = PWM(Pin(14))               # Motor connected to GPIO 14 (PWM)
+# pir_sensor = Pin(16, Pin.IN)         # PIR sensor connected to GPIO 16
+# led = Pin(15, Pin.OUT)               # LED connected to GPIO 15
+motor1a = Pin(14, Pin.OUT)             # Motor IN1 connected to GPIO 14
+motor1b = Pin(13, Pin.OUT)             # Motor IN2 connected to GPIO 13
+motor_pwm = PWM(Pin(12))               # PWM pin for speed control on GPIO 12
 motor_pwm.freq(1000)                   # Set PWM frequency for motor
+uart_id = 0                            # UART ID
+uart = UART(uart_id, baudrate=9600, tx=Pin(0), rx=Pin(1))  # TX (Pin 0), RX (Pin 1)
 
-# Setup UART for DFPlayer Mini
-uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))  # TX (Pin 0), RX (Pin 1)
+# Create DFPlayer instance, using the UART ID, and pin IDs
+music = DFPlayer(uart_id, tx_pin_id=0, rx_pin_id=1)
 
-# DFPlayer Mini commands
-def play_song():
-    uart.write(b'\x7E\xFF\x06\x03\x00\x00\x01\xFE\xF7\xEF')  # Play first song
-    time.sleep(10)  # Play for 10 seconds
-    uart.write(b'\x7E\xFF\x06\x16\x00\x00\x00\xFE\xE9\xEF')  # Stop playing
+# Give dfplayer time to initialize
+time.sleep(0.2) 
 
-# Motor control
-def spin_motor():
-    motor_pwm.duty_u16(32768)  # Set motor to 50% duty cycle (adjust if needed)
-    time.sleep(10)             # Spin for 10 seconds
-    motor_pwm.duty_u16(0)      # Stop motor
+# Set volume
+music.volume(10)
+
+# Play a song
+def play_song(folder, track):
+    print(f"Playing track {track} from folder {folder}")
+    music.play(folder, track)
+    sleep(11)  # Play for 11 seconds
+
+
+# Motor control functions
+def spin_motor_forward():
+    print("Spinning motor")
+    for _ in range(5):  # Spin 5 times
+        print("Spinning motor")
+        start_motor()
+        time.sleep(1)                    # Spin for 1 second
+        stop_motor()                     # Stop the motor
+        time.sleep(1)                    # Pause for 1 second
+
+# Spin the motor forwards
+def start_motor():
+    motor1a.high()                       # Set direction to forward
+    motor1b.low()
+    motor_pwm.duty_u16(32768)            # Set motor to 50% duty cycle
+
+# Stop the motor
+def stop_motor():
+    motor1a.low()
+    motor1b.low()
 
 # LED flash control
 def flash_led():
-    for _ in range(10):        # Flash for 10 seconds (1 second interval)
+    for _ in range(10):                  # Flash for 10 seconds (1 second interval)
         led.on()
         time.sleep(0.5)
         led.off()
@@ -34,11 +62,14 @@ def flash_led():
 
 # Main loop
 while True:
-    if pir_sensor.value() == 1:    # Detect motion
+#    if pir_sensor.value():              # Check if motion is detected
+    if 2 > 1:                            # Currently set to execute continuously for testing
+        stop_motor()
         print("Motion detected!")
-        play_song()                # Play song for 10 seconds
-        spin_motor()               # Spin motor for 10 seconds
-        flash_led()                # Flash LED for 10 seconds
-        print("Pausing for 5 seconds...")
-        time.sleep(15)              # Pause for 15 seconds before checking again
-    time.sleep(0.1)                # Short delay before checking again
+#        flash_led()                     # Flash LED when motion is detected
+        spin_motor_forward()             # Spin motor forward
+        play_song(1,1)                   # Play track 1 from folder 1
+        stop_motor()
+        
+    print("Sleeping for 15 seconds...")
+    time.sleep(15)                      # Pause before checking for event again
--- README.md
+++ README.md
@@ -9,10 +9,26 @@ This repository contains instructions and code for building your own shaking gho
 - Adjustable on/sleep durations
 - Design your own ghost face
 - 3D printable enclosure or get creative
-- Roughly $38 USD to build
 
-## Hardware
-
+## Parts List
+- Raspberry Pi Pico H RP2040 Microcontroller ($9 presoldered)
+- CQRobot 3W 4Ohm 4O3W-JST-PH2.0 Speakers ($8) - Only need 1 of the 2 speakers
+- EUDAX 6 Set 3-12V DC Motors Kit & Battery Packs ($12) - Only need 1 of the 6 motors and battery packs
+- DFPlayer Mini ($10)
+- Micro SD Card ($5 or less for 1GB which is more than enough)
+- HiLetgo 3pcs HC-SR501 PIR Infrared Sensors ($8.50) - Only need 1 of the 3 sensors
+- KOOBOOK 5Pcs DRV8833 Motor Drive Module ($8) - Only need 1 of the 5 motor drivers
+- White square cloth
+- 470uF 10V Capacitors (maybe optional)
+- Breadboard (optional)
+- Jumper cables
+- 0.1uF Capacitors (probably optional)
+- Soldering iron
+- 3D printer and filament (optional)
+- AA batteries (2 count)
+- USB power bank (maybe, TBD)
+<br>
+Total cost: ~$60 but you'll almost end up with enough extra parts to build 3 whole ghosts, assuming you already have a soldering iron, wires, capacitors, and cloth. Also you can opt for a 4 pack of unsoldered RPi Pico for $20.
 
 ## Setup
 
